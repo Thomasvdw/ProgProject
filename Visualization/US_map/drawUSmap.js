c4a490f5c61a8ba670a80b11b7518abcddca541a
Thomas van der Wardt <thomas_vdw@live.nl>
2015-06-05 13:09:17 +0200

    update

--- Visualization/US_map/drawUSmap.js
+++ Visualization/US_map/drawUSmap.js
@@ -36,13 +36,15 @@ var state_ids = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI'
 				.attr("height", height)
 				.attr("class", "visualization");
 			
-			svg.append("rect")
-				.attr("x", 0)
-				.attr("y", 50)
-				.attr("width", 150)
-				.attr("height", 200)
-				.attr("stroke", "black")
-				.attr("fill", "transparent")
+			var data_reference = svg.insert("g");
+				data_reference.append("rect")
+					.attr("x", 0)
+					.attr("y", 50)
+					.attr("width", 150)
+					.attr("height", 100)
+					.attr("stroke", "black")
+					.attr("fill", "transparent")
+			
 
 			d3.json("us.json", function(unitedState) {
 				var data = topojson.feature(unitedState, unitedState.objects.states).features;
@@ -71,56 +73,43 @@ var state_ids = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI'
 					.attr("d", path)
 					.attr("stroke", "black")
 					.on("mouseover", function(d) {
-						//console.log(fullNames[d.id]);
+						var texts = ["State: ", "Growth: ", "Capacity: ", "Population: "];
+						
+						d3.selectAll("text").remove()
+						for (var j = 0; j < texts.length; j++){
+							data_reference.append("text")
+								.text(texts[j])
+								.attr("x", 5)
+								.attr("y", 65 + 23 * j)
+								.attr("font-family", "Verdana")
+								.attr("font-size", "12.5");
+						}
+
+						var growth = parseFloat((parseFloat(parseFloat(nameSize[nameCodes[d.id]][15].Size) - parseFloat(nameSize[nameCodes[d.id]][16].Size))/parseFloat(nameSize[nameCodes[d.id]][16].Size)));
+						var popup_texts = [fullNames[d.id], String(growth * 100).substr(0,5) + " %", parseFloat(nameSize[nameCodes[d.id]][15].Size) + " MW", parseFloat(namePopulation[nameCodes[d.id]][19].Population) + " mln"];
+							
+						for (var i = 0; i < popup_texts.length; i++){
+							data_reference.append("text")
+								.text(popup_texts[i])
+								.attr("x", 70)
+								.attr("y", 65 + 23 * i)
+								.attr("font-family", "Verdana")
+								.attr("font-size", "12.5");
+						}
 					})
 					.attr("class", function(d,i) { 
 						return nameCodes[d.id];
 					})
-					.attr("fill", function(d,i) {
-						if (nameSize[nameCodes[d.id]] != undefined){
-							var size = parseFloat(nameSize[nameCodes[d.id]][15].Size);
-							var population = parseFloat(namePopulation[nameCodes[d.id]][19].Population);
-								
-								
-							// Find reference values: method #1 (% of max_size)
-							/*
-							var size_array = [];
-							for (var i = 0; i < nameSize[nameCodes[d.id]].length; i++){
-								size_array.push(parseInt(nameSize[nameCodes[d.id]][i].Size));
-							}
-							var max_size = Math.max.apply(null, size_array);
-							var referencepopulation = parseFloat(namePopulation[nameCodes[d.id]][19].Population);
-							var referenceSizePerCapita = parseFloat(max_size / referencepopulation);
-							var sizePerCapita = parseFloat(size / population);
-							var relativeAmount = parseFloat(sizePerCapita / referenceSizePerCapita);
-							relativeAmount = parseFloat(size / max_size);
-							
-							
-							if (isNaN(relativeAmount)){
-								return "grey";
-							} 
-							*/
-							// Reference values: method #2 (year growth)					
-							var size_before = parseFloat(nameSize[nameCodes[d.id]][16].Size);
-							var size_difference = parseFloat(size - size_before);
-							var growth = parseFloat(size_difference / size_before);
-									
-							if (growth > 1){
-								growth = 1;
-							}	
-							return d3.interpolate("#f7fcfd", "#00441b")(growth);	
-						};
-						return "#f7fcfd"
-					});	
+					.attr("fill", "#f7fcfd");
 				
 				setTimeout(recolor, 1000);
 				
-				var startSize = 14
-				var startDate = 20
+				var startSize = 14;
+				var startDate = 20;
 				
 				function recolor(){
 					
-					if (startSize > 0){
+					if (startSize >= 0){
 					
 						svg.append("g")
 						.attr("class", "states-bundle")
@@ -130,8 +119,49 @@ var state_ids = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI'
 						.append("path")
 						.attr("d", path)
 						.on("mouseover", function(d) {
-							console.log(fullNames[d.id]);
-							console.log(nameSize[nameCodes[d.id]][startSize + 1].Size);
+							
+							var size = parseFloat(nameSize[nameCodes[d.id]][startSize].Size);
+							var population = parseFloat(namePopulation[nameCodes[d.id]][startDate].Population);
+							var size_before = parseFloat(nameSize[nameCodes[d.id]][startSize + 1].Size);
+							var size_difference = parseFloat(size - size_before);
+							var growth = parseFloat(size_difference / size_before);	
+							
+							var texts = ["State: ", "Growth: ", "Capacity: ", "Population: "];
+							
+							d3.selectAll("text").remove()
+							for (var j = 0; j < texts.length; j++){
+								var font_size = "12.5";
+								if (i == 3){
+									font_size = "11";
+								}
+								data_reference.append("text")
+									.text(texts[j])
+									.attr("x", 5)
+									.attr("y", 65 + 23 * j)
+									.attr("font-family", "Verdana")
+									.attr("font-size", font_size);
+							}
+							
+							var popup_texts = [fullNames[d.id], String(growth * 100).substr(0,5) + " %", parseFloat(nameSize[nameCodes[d.id]][startSize].Size) + " MW", parseFloat(namePopulation[nameCodes[d.id]][startDate].Population) + " mln"];
+							
+							for (var i = 0; i < popup_texts.length; i++){
+								var j = 0;
+								var font_size = "12.5";
+								if (i == 0){
+									j -= 20
+								}
+								if (i == 3){
+									j += 10;
+									font_size = "11";
+								}
+								data_reference.append("text")
+									.text(popup_texts[i])
+									.attr("x", 70 + j)
+									.attr("y", 65 + 23 * i)
+									.attr("font-family", "Verdana")
+									.attr("font-size", font_size);
+								j = 0;
+							}
 						})
 						.attr("stroke", "black")
 						.attr("fill", function(d,i) {
@@ -142,21 +172,37 @@ var state_ids = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI'
 								var size_difference = parseFloat(size - size_before);
 								var growth = parseFloat(size_difference / size_before);						
 								
+								if (isNaN(growth)){
+									growth = 0;
+								}
+								
 								if (growth > 1){
 									growth = 1;
 								}
+								
+								console.log(growth);
 								return d3.interpolate("#f7fcfd", "#00441b")(growth);
 							}
-							
 							var growth = 0;
-							
 							return d3.interpolate("#f7fcfd", "#00441b")(growth);
 						});
+						if (startSize != 0){
+							startSize -= 1;
+							startDate += 1;
+							clearTimeout();
+							setTimeout(recolor, 2500);
+						}
+						/*
+						else {
+							// Restart?
+							alert("Restart?");
+							startSize = 15;
+							startDate = 19;
+							clearTimeout();
+							setTimeout(recolor, 200);
+						}
+						*/
 					}
-					startSize -= 1;
-					startDate += 1;
-					clearTimeout();
-					setTimeout(recolor, 300);
 				}
 				});
 			});
